1e4cb58ea0c4519157f0db640f904d1db24d1786
AMIT KUMAR <120617868+shekeramit@users.noreply.github.com>
2024-12-08 11:50:01 +0530

    Update CountDownTimer.jsx

--- src/components/HeroPage/CountDownTimer.jsx
+++ src/components/HeroPage/CountDownTimer.jsx
@@ -9,13 +9,19 @@ export const CountDownTimer = () => {
     const difference = eventDate - currentTime;
 
     let timeLeft = {};
-    if (difference > 0) {
-      timeLeft = {
-        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-        minutes: Math.floor((difference / 1000 / 60) % 60),
-        seconds: Math.floor((difference / 1000) % 60),
-      };
+    // if (difference > 0) {
+    //   timeLeft = {
+    //     days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+    //     hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+    //     minutes: Math.floor((difference / 1000 / 60) % 60),
+    //     seconds: Math.floor((difference / 1000) % 60),
+    //   };
+    // }
+    timeLeft = {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
     }
     return timeLeft;
   };
